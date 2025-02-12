e5a8a04479835b61047ed63f8bf6d9fd9a5d31dc
Natan Menegasse <natan.menegasse@hotmail.com>
2025-02-12 15:00:01 -0300

    Lets get started!

---
+++ main.py
@@ -0,0 +1,129 @@
+import os
+import json
+import math
+
+from enum import StrEnum
+from typing import Optional, NamedTuple
+from dataclasses import dataclass
+
+CWD = os.getcwd()
+
+Inss_Range = NamedTuple("Inss_Range", [("initial", float), ("final", float), ("aliquot", float)])
+Income_Tax_Range = NamedTuple("Income_Tax_Range", [("initial", float), ("final", float), ("aliquot", float), ("monthly_deduction", float)])
+
+type ISO_Date_Format = str
+type Inss_Table = dict[ISO_Date_Format, dict[str, Inss_Range]]
+type Income_Tax_Table = dict[ISO_Date_Format, dict[str, Income_Tax_Range]]
+
+class Currency(StrEnum):
+    Real = "Real (R$)"
+    Dollar = "Dollar (US$)"
+
+inss_table = {
+    "first_band": Inss_Range(0.0, 1518.0, 0.075),
+    "second_band": Inss_Range(1518.01, 2793.88, 0.09),
+    "third_band": Inss_Range(2793.89, 4190.83, 0.12),
+    "foruth_band": Inss_Range(4190.84, 8157.41, 0.14)
+}
+
+income_tax_table = {
+    "first_band": Income_Tax_Range(0.0, 2259.20, 0.0, 0.0),
+    "second_band": Income_Tax_Range(2259.21, 2828.65, 0.075, 169.44),
+    "third_band": Income_Tax_Range(2828.66, 3751.05, 0.15, 381.44),
+    "fourth_band": Income_Tax_Range(3751.06, 4664.68, 0.225, 662.77),
+    "fith_band": Income_Tax_Range(4664.69, math.inf, 0.275, 896.00)
+}
+
+def inss_deduction(salary: float, inss_table: Inss_Table) -> float:
+    aliquot_sum: float = 0.0
+    for inss_range in inss_table.values():
+        if salary >= inss_range.final:
+            aliquot_sum += (inss_range.final - inss_range.initial) * inss_range.aliquot
+        else:
+            aliquot_sum += (salary - inss_range.initial) * inss_range.aliquot
+    return aliquot_sum
+
+def income_tax_deduction(salary: float, income_tax_table: Income_Tax_Table, inss_deduction: float) -> float:
+    income_tax_sum: float = 0.0
+    for income_tax_range in income_tax_table.values():
+        if income_tax_range.initial <= salary and salary <= income_tax_range.final:
+            income_tax_sum = ((salary - inss_deduction) * income_tax_range.aliquot) - income_tax_range.monthly_deduction
+            break
+    return income_tax_sum
+
+@dataclass
+class Static_Financial_Status:
+
+    @dataclass
+    class Planned_Work_Hours:
+        per_day: float
+        per_week: float
+        per_month: float
+        per_year: float
+
+    @dataclass
+    class Planned_Salary:
+        per_day: Optional[float] = None
+        per_week: Optional[float] = None
+        per_month: Optional[float] = None
+        per_year: Optional[float] = None
+
+    latest: bool
+    starting_date: ISO_Date_Format
+    ending_date: ISO_Date_Format
+    income_tax_percentage: float
+    planned_work_hours: Planned_Work_Hours
+    planned_salary: Planned_Salary
+    salary_currency: Currency
+    inss_table: list[Inss_Table]
+
+    def to_json(self) -> dict[str, ...]:
+        return {
+            "latest": self.latest,
+            "salary_currency": self.salary_currency.value,
+            "starting_date": self.starting_date,
+            "ending_date": self.ending_date,
+            "income_tax_percentage": self.income_tax_percentage,
+            "planned_work_hours": {
+                "per_day": self.planned_work_hours.per_day,
+                "per_week": self.planned_work_hours.per_week,
+                "per_month": self.planned_work_hours.per_month,
+                "per_year": self.planned_work_hours.per_year
+            },
+            "planned_salary": {
+                "per_month": self.planned_salary.per_month
+            }
+        }
+
+@dataclass
+class Volatile_Financial_Status:
+    ...
+
+if __name__ == "__main__":
+    filename: str = "financial_data.json"
+    filename_path: str = os.path.join(CWD, filename)
+    if not os.path.exists(filename_path):
+        with open(filename_path, "w") as stream:
+            json.dump({}, stream)
+
+    financial_status = Static_Financial_Status(
+        latest=True,
+        starting_date="2024-01-01",
+        ending_date="2025-01-01",
+        income_tax_percentage=0.275,
+        inss_table=inss_table,
+        planned_work_hours=Static_Financial_Status.Planned_Work_Hours(
+            per_day=10.0, per_week=40.0, per_month=160.0, per_year=1920
+        ),
+        planned_salary=Static_Financial_Status.Planned_Salary(
+            per_month=10000.0
+        ),
+        salary_currency=Currency.Real
+    )
+
+    with open(filename_path, "w") as stream:
+        json.dump(financial_status.to_json(), stream, indent=4)
+
+    salary = 10000
+    inss_deduction_val = inss_deduction(salary, inss_table)
+    print(inss_deduction_val, income_tax_deduction(salary, income_tax_table, inss_deduction_val))
