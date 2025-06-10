473c45de62d36bf06a4bf4bcc4faf64335b390ea
Natan Menegasse <natan.menegasse@hotmail.com>
2025-06-09 21:45:24 -0300

    A few new things

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python3
+
+from __future__ import annotations
+
 import os
+import sys
 import json
 import math
+import uuid
+import toml
 
+from dateutil.relativedelta import relativedelta
+from datetime import datetime, timedelta
 from enum import StrEnum
-from typing import Optional, NamedTuple
+from typing import NamedTuple
 from dataclasses import dataclass, field
+from collections.abc import Callable
 
 CWD = os.getcwd()
 
@@ -14,6 +24,8 @@ Income_Tax_Range = NamedTuple("Income_Tax_Range", [("initial", float), ("final",
 type ISO_Date_Format = str
 type Inss_Table = dict[ISO_Date_Format, dict[str, Inss_Range]]
 type Income_Tax_Table = dict[ISO_Date_Format, dict[str, Income_Tax_Range]]
+
+type Monthly_Icome_Callable = Callable[[..., float], float]
 
 class Currency(StrEnum):
     Real = "Real (R$)"
@@ -55,7 +67,7 @@ def income_tax_deduction_calculation(gross_salary: float, income_tax_table: Inco
 class Static_Financial_Status:
     latest: bool
     starting_date: ISO_Date_Format
-    ending_date: ISO_Date_Format
+    ending_date: ISO_Date_Format | None
     gross_salary: float
     net_salary: float = field(init=False)
     inss_table: Inss_Table
@@ -66,6 +78,8 @@ class Static_Financial_Status:
     working_hours_per_day: float = field(repr=False)
 
     def __post_init__(self):
+        if self.latest is None: self.latest = False
+
         self.inss_deduction = inss_deduction_calculation(self.gross_salary, self.inss_table)
         self.income_tax_deduction = income_tax_deduction_calculation(self.gross_salary, self.income_tax_table, self.inss_deduction)
         self.net_salary = self.gross_salary - self.inss_deduction - self.income_tax_deduction
@@ -82,11 +96,7 @@ class Static_Financial_Status:
             "income_tax_deduction": self.income_tax_deduction
         }
 
-@dataclass
-class Volatile_Financial_Status:
-    ...
-
-if __name__ == "__main__":
+def run_static_financial_status():
     filename: str = "financial_data.json"
     filename_path: str = os.path.join(CWD, filename)
     if not os.path.exists(filename_path):
@@ -106,4 +116,138 @@ if __name__ == "__main__":
 
     with open(filename_path, "w") as stream:
         json.dump(financial_status.to_json(), stream, indent=4)
+
+def new_card(id: str, card_type: str, card_name: str, payment_type: str, final_number: str):
+    return {
+        "id": id,
+        "type": card_type,
+        "name": card_name,
+        "payment_type": payment_type,
+        "final_number": final_number,
+    }
+
+def new_sale(status: str, card_id: str, sale_id: str, name: str, sale_date: str, currency: str, sale_value: float, installments: int):
+    return {
+        "status": status,
+        "card_id": card_id,
+        "sale_id": sale_id,
+        "name": name,
+        "sale_date": sale_date,
+        "currency": currency,
+        "sale_value": sale_value,
+        "installments": installments,
+    }
+
+def new_installments(sale):
+    installments = sale["installments"]
+    sale_value = sale["sale_value"]
+    monthly_amount = sale_value / installments
+    result = []
+    for i in range(installments):
+        i += 1
+        installment_id = str(uuid.uuid4())
+        result.append({
+            "sale_id": sale["sale_id"],
+            "id": installment_id,
+            "installment": i,
+            "value": monthly_amount,
+            "date": "..."
+        })
+    return result
+
+def generate_date_range(start: datetime, end: datetime) -> list[datetime]:
+    create_date_range: list[datetime] = []
+    current = start
+    while current <= end:
+        create_date_range.append(current)
+        current += relativedelta(months=1)
+    return create_date_range
+
+def generate_yearly_income_per_month(date_range: list[datetime], total_gross_income: float) -> dict[str, float]:
+    result = {}
+    for date in date_range:
+        key: str = f"{date.year}-{date.month:02}"
+        result[key] = total_gross_income
+    return result
+
+def estimate_yearly_income_100(**args) -> dict[str, dict[str, float]]:
+    assert (start := args.get("start")) is not None, "Expected the field 'start: datetime' but was not found"
+    assert (end := args.get("end")) is not None, "Expected the field 'end: datetime' but was not found"
+    assert (monthly_gross_salary_income := args.get("monthly_gross_salary_income")) is not None, "Expected the field 'monthly_gross_salary_income: float' but was not found"
+    assert (monthly_gross_food_income := args.get("monthly_gross_food_income")) is not None, "Expected the field 'monthly_gross_food_income: float' but was not found"
+
+    yearly_gross_salary_income: float = monthly_gross_salary_income * 12 + monthly_gross_salary_income
+    yearly_gross_food_income: float = monthly_gross_food_income * 12
+    total_gross_income: float = yearly_gross_salary_income + yearly_gross_food_income
+
+    date_range = generate_date_range(start, end)
+    result = generate_yearly_income_per_month(date_range, total_gross_income)
+    return result
+
+def estimate_yearly_income_200(**args) -> dict[str, dict[str, float]]:
+    assert (start := args.get("start")) is not None, "Expected the field 'start: datetime' but was not found"
+    assert (end := args.get("end")) is not None, "Expected the field 'end: datetime' but was not found"
+    assert (monthly_gross_salary_income := args.get("monthly_gross_salary_income")) is not None, "Expected the field 'monthly_gross_salary_income: float' but was not found"
+    assert (monthly_gross_food_income := args.get("monthly_gross_food_income")) is not None, "Expected the field 'monthly_gross_food_income: float' but was not found"
+    assert (monthly_benefit_income := args.get("monthly_benefit_income")) is not None, "Expected the field 'monthly_benefit_income: float' but was not found"
+
+    yearly_gross_salary_income: float = monthly_gross_salary_income * 12 + monthly_gross_salary_income
+    yearly_gross_food_income: float = monthly_gross_food_income * 12
+    yearly_benefit_income: float = monthly_benefit_income * 12
+    total_gross_income: float = yearly_gross_salary_income + yearly_gross_food_income + yearly_benefit_income
+
+    date_range = generate_date_range(start, end)
+    result = generate_yearly_income_per_month(date_range, total_gross_income)
+    return result
+
+
+estimate_yearly_income = {
+    "1.0.0": estimate_yearly_income_100,
+    "2.0.0": estimate_yearly_income_200,
+}
+
+def estimate_yearly_income_overtime(version: str, **args) -> dict[str, dict[str, float]]:
+    assert (callback := estimate_yearly_income.get(version, None)) is not None, f"Invalid version number {version}"
+    result = callback(**args)
+    return result
+
+def load_income_estimation_configuration(filepath: str) -> list[dict[str, str | float]]:
+    gross_income_estimation_configuration = []
+    with open(filepath, "r") as stream:
+        data = toml.load(stream)
+        for config in data["income_estimation"]:
+            if config["version"] == "1.0.0":
+                start = datetime.strptime(config["start"], "%Y-%d-%m")
+                end = datetime.strptime(config["end"], "%Y-%d-%m")
+                gross_income_estimation_configuration.append({
+                    "version": config["version"],
+                    "start": start,
+                    "end": end,
+                    "monthly_gross_salary_income": config["monthly_gross_salary_income"],
+                    "monthly_gross_food_income": config["monthly_gross_food_income"],
+                })
+            elif config["version"] == "2.0.0":
+                start = datetime.strptime(config["start"], "%Y-%d-%m")
+                end = datetime.strptime(config["end"], "%Y-%d-%m") if config["end"] != "today" else datetime.today()
+                gross_income_estimation_configuration.append({
+                    "version": config["version"],
+                    "start": start,
+                    "end": end,
+                    "monthly_gross_salary_income": config["monthly_gross_salary_income"],
+                    "monthly_gross_food_income": config["monthly_gross_food_income"],
+                    "monthly_benefit_income": config["monthly_benefit_income"]
+                })
+    return gross_income_estimation_configuration
+
+if __name__ == "__main__":
+    gross_income_estimation_configuration = load_income_estimation_configuration("././income_estimation_configuration.toml")
+
+    result = {}
+    for income_entry in gross_income_estimation_configuration:
+        result.update(estimate_yearly_income_overtime(**income_entry))
+
+    MONTHS_WORKED = 12
+    WEEKS_WORKED = 4
+    DAYS_WORKED = 5
+    HOURS_WORKED = 8
 
